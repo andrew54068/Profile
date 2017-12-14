08f617e1925d44377f2a776e13360fab314c5fcb
andrew54068 <andrew0424718012@yahoo.com.tw>
2017-12-14 17:01:28 +0800

    ##setting work##
    issue
    1. Header of change password been clicked will trigger cell below.
    2. Profile need to fix

---
+++ Profile/SettingContent_TVCell.swift
@@ -0,0 +1,28 @@
+//
+//  SettingContent_TVCell.swift
+//  Profile
+//
+//  Created by kidnapper on 14/12/2017.
+//  Copyright © 2017 25sprout.com. All rights reserved.
+//
+
+import UIKit
+
+class SettingContent_TVCell: UITableViewCell {
+
+    @IBOutlet var contentTitle: UILabel!
+    @IBOutlet var contentSwitch: UISwitch!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Profile/menu_TVCell.swift
@@ -0,0 +1,30 @@
+//
+//  menu_TVCell.swift
+//  Profile
+//
+//  Created by kidnapper on 14/12/2017.
+//  Copyright © 2017 25sprout.com. All rights reserved.
+//
+
+import UIKit
+
+class menu_TVCell: UITableViewCell {
+    
+    @IBOutlet var thumbnail: UIImageView!
+    
+    @IBOutlet var menuItemLabel: UILabel!
+    
+    @IBOutlet var arrow: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
